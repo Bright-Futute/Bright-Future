484f2ee21735183167a6c64756e3242419d0c49c
obaidul islam1 <mursalinislam537215@gmail.com>
2023-02-03 18:55:45 +0600

    repository setup complete

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Services = () => {
   return (
     <section className="py-52">
-      <h2 className="text-center">Services </h2>
+      <h2 className="text-center">Services page obaidul islam</h2>
     </section>
   );
 };
