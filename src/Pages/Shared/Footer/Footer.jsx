78b4f899a8d02ca8d4b8de022ca9553d64de943f
musfiqurofficial <musfiqurofficial1999@gmail.com>
2023-02-16 10:43:07 +0600

    add file

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import Logo from "../Navbar/bright.png";
 const Footer = () => {
   return (
     <section>
-      <div className="relative mt-16 bg-deep-purple-400">
+      <div className="relative mt-16 bg-gray-900">
         <svg
-          className="absolute top-0 w-full h-6 -mt-5 sm:-mt-10 sm:h-16 text-deep-purple-400"
+          className="absolute top-0 w-full h-6 -mt-5 sm:-mt-10 sm:h-16 text-gray-900"
           preserveAspectRatio="none"
           viewBox="0 0 1440 54"
         >
@@ -26,11 +26,11 @@ const Footer = () => {
                 <img className="h-[60px]" src={Logo} alt="" />
               </a>
               <div className="mt-4 lg:max-w-sm">
-                <p className="text-sm text-deep-purple-50">
+                <p className="text-sm text-purple-50">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem aperiam.
                 </p>
-                <p className="mt-4 text-sm text-deep-purple-50">
+                <p className="mt-4 text-sm text-purple-50">
                   Eaque ipsa quae ab illo inventore veritatis et quasi
                   architecto beatae vitae dicta sunt explicabo.
                 </p>
@@ -45,7 +45,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       News
                     </a>
@@ -53,7 +53,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       World
                     </a>
@@ -61,7 +61,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Games
                     </a>
@@ -69,7 +69,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       References
                     </a>
@@ -84,7 +84,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Web
                     </a>
@@ -92,7 +92,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       eCommerce
                     </a>
@@ -100,7 +100,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Business
                     </a>
@@ -108,7 +108,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Entertainment
                     </a>
@@ -116,7 +116,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Portfolio
                     </a>
@@ -131,7 +131,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Media
                     </a>
@@ -139,7 +139,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Brochure
                     </a>
@@ -147,7 +147,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Nonprofit
                     </a>
@@ -155,7 +155,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Educational
                     </a>
@@ -163,7 +163,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Projects
                     </a>
@@ -178,7 +178,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Infopreneur
                     </a>
@@ -186,7 +186,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Personal
                     </a>
@@ -194,7 +194,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Wiki
                     </a>
@@ -202,7 +202,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="/"
-                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-400"
+                      className="transition-colors duration-300 text-purple-50 hover:text-teal-400"
                     >
                       Forum
                     </a>
@@ -211,14 +211,14 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col justify-between pt-5 pb-10 border-t border-deep-purple-200 sm:flex-row">
+          <div className="flex flex-col justify-between pt-5 pb-10 border-t border-purple-200 sm:flex-row">
             <p className="text-sm text-gray-100">
               © Copyright 2020 Lorem Inc. All rights reserved.
             </p>
             <div className="flex items-center mt-4 space-x-4 sm:mt-0">
               <a
                 href="/"
-                className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-400"
+                className="transition-colors duration-300 text-purple-100 hover:text-teal-400"
               >
                 <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                   <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
@@ -226,7 +226,7 @@ const Footer = () => {
               </a>
               <a
                 href="/"
-                className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-400"
+                className="transition-colors duration-300 text-purple-100 hover:text-teal-400"
               >
                 <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
                   <circle cx="15" cy="15" r="4" />
@@ -235,7 +235,7 @@ const Footer = () => {
               </a>
               <a
                 href="/"
-                className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-400"
+                className="transition-colors duration-300 text-purple-100 hover:text-teal-400"
               >
                 <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                   <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
