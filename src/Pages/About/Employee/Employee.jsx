323e6a19c7ff13dbae921c1f93eff0a04700a4c0
musfiqurofficial <musfiqurofficial1999@gmail.com>
2023-02-18 22:59:15 +0600

    Card Design, json employee demo data create

--- src/Pages/About/Employee/Employee.jsx
+++ src/Pages/About/Employee/Employee.jsx
@@ -21,41 +21,29 @@ const Employee = () => {
   }, []);
   return (
     <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-      <div className="block lg:flex justify-between items-center">
-        <div className=" lg:w-[45%]">
-          <h2 className="text-4xl font-bold font-mono pb-5 text-[#3B80B8] drop-shadow-md">
-            Our Team
-          </h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas,
-            deleniti nemo nulla quod et laboriosam inventore tempore dolorem
-            corrupti eveniet hic saepe, ipsam fugiat, architecto nihil ipsum
-            dolorum officiis aliquam quasi! Tempore, maxime tenetur reiciendis
-            sint sed voluptate qui blanditiis placeat veritatis perspiciatis rem
-            illo vitae porro voluptas, fugiat debitis?
-          </p>
-        </div>
-        <div className="mx-auto">
-          <Swiper
-            effect={"cards"}
-            grabCursor={true}
-            modules={[EffectCards]}
-            className="mySwiper"
-          >
-            {employees?.map((employee) => (
-              <SwiperSlide>
-                <EmployeeCard
-                  key={employee._id}
-                  employee={employee}
-                ></EmployeeCard>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
+      <div className="text-center mb-8">
+        <h2 className="text-4xl font-bold font-mono pb-5 text-[#3B80B8] drop-shadow-md">
+          Our Team
+        </h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
       </div>
-      {/* {employees?.map((employee) => (
-        <EmployeeCard key={employee._id} employee={employee}></EmployeeCard>
-      ))} */}
+      <div className="mx-auto">
+        <Swiper
+          effect={"cards"}
+          grabCursor={true}
+          modules={[EffectCards]}
+          className="mySwiper"
+        >
+          {employees?.map((employee) => (
+            <SwiperSlide>
+              <EmployeeCard
+                key={employee._id}
+                employee={employee}
+              ></EmployeeCard>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
--- src/Pages/About/Employee/EmployeeCard.jsx
+++ src/Pages/About/Employee/EmployeeCard.jsx
@@ -1,25 +1,28 @@
 import React from "react";
+import "./employeeCadCSS.css";
+import { BsLinkedin } from "react-icons/bs";
 
 const EmployeeCard = ({ employee }) => {
   const { name, position, linkedin, img } = employee;
+
+  // const bgImg = {
+  //   backgroundImage: `url(${img})`,
+  //   backgroundColor: "#cccccc",
+  //   height: "500px",
+  //   backgroundPosition: "center",
+  //   backgroundRepeat: "no-repeat",
+  //   backgroundSize: "cover",
+  // };
   return (
-    <div className="flex flex-col">
-      <div className="w-[45%] mx-auto">
-        <img src={img} alt="" />
+    <div className="card">
+      <img src={img} alt="Employee" className="card-img p-0" />
+      <div className="card-body text-white flex flex-col justify-center text-center font-mono">
+        <h3 className="text-3xl font-bold text-white">{name}</h3>
+        <h5 >{position}</h5>
+        <a href={linkedin} className="mx-auto mt-5">
+          <BsLinkedin className="w-10 h-10" />
+        </a>
       </div>
-      <div className="text-center">
-        <h3>{name}</h3>
-        <h5>{position}</h5>
-        <a href={linkedin}>Linkedin</a>
-      </div>
-      {/* <figure className="">
-        <img src={img} alt="" className="rounded-xl mx-auto w-max h-20" />
-      </figure>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title">{name}</h2>
-        <p className="text-sm font-light">{position}</p>
-        <a href={linkedin}>Linkedin</a>
-      </div> */}
     </div>
   );
 };
