ae95e6066fe75d1eb7ccfac8351bbcc48b0ee0de
musfiqurofficial <musfiqurofficial1999@gmail.com>
2023-02-19 22:11:49 +0600

    Check

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Main from "./Layout/Main/Main";
+import router from "./Router/Router";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <div>
+      {/* <BrowserRouter>
         <Main></Main>
-      </BrowserRouter>
+      </BrowserRouter> */}
+
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Pages/Home/Projects/ProjectDetails.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ProjectDetails = () => {
+  return (
+    <div>
+      <h2>Hello from </h2>
+      <Link to="/">
+        <button>Back to Home</button>
+      </Link>
+    </div>
+  );
+};
+
+export default ProjectDetails;
